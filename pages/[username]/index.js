844392e0385c3e7158a0694de21d2fc743af4f6e
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2022-11-22 09:57:14 +0800

    second commit

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import PostFeed from '../../components/PostFeed'
+import UserProfile from '../../components/UserProfile'
 
-export default function UserProfilePage() {
+export async function getServerSideProps({ query }) {
+  //get the username from URL
+  const { username } = query
+
+  //fetch the user document based on that username
+  const userDoc = await getUserWithUsername(username)
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }
+  }
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
-    <div>index</div>
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
 
-import Loader from '../components/Loader'
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-export default function Home() {
+import { useState } from 'react';
+
+const LIMIT = 1
+
+export async function getServerSideProps() {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts)
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Loader show={true} />
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
   )
 }
